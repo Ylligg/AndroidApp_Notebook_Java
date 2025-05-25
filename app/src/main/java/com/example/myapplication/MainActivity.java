90f7772983f3ef9f2bc5bf137137b7d82e5d0e79
ylli <yll.gashi@hotmail.com>
2025-05-25 10:48:20 +0200

    added recyclerview, not yet working but added note cards and shaping the app structure

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +24,25 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.notes);
+        arrayList = new ArrayList<String>();
+
+        arrayList.add("Hei1");
+        arrayList.add("Hei2");
+        arrayList.add("Hei3");
+        arrayList.add("Hei");
+        arrayList.add("Hei");
 
 
 
+        MyAdapter myAdapter = new MyAdapter(this, arrayList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(myAdapter);
+
+
+    }
+
+    public void additem(View view){
+        arrayList.add("Hei");
     }
 }
---
+++ app/src/main/java/com/example/myapplication/MyAdapter.java
@@ -0,0 +1,66 @@
+package com.example.myapplication;
+
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
+    Context context;
+
+
+    ArrayList<String> notes;
+
+
+    public MyAdapter(Context context, ArrayList<String> notes){
+        this.context = context;
+        this.notes = notes;
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.notecard, parent, false);
+
+        return new MyViewHolder(view).linkAdpater(this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
+        holder.navn.setText(notes.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView navn;
+        private MyAdapter adapter;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            navn = itemView.findViewById(R.id.navn);
+
+
+        }
+
+        public MyViewHolder linkAdpater(MyAdapter adapter){
+            this.adapter = adapter;
+            return this;
+        }
+
+    }
+}
